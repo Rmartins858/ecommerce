003571031f9aca7364d5707f38a76082d103877d
Rafael Martins <rmartins858@gmail.com>
2022-07-27 17:22:29 -0300

    Adding filters

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -1,64 +1,114 @@
 import Annoucement from '../components/Annoucement';
-import styled from "styled-components"
-import Navbar from "../components/Navbar"
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
-const Container = styled.div``
+const Container = styled.div``;
 const Wrapper = styled.div`
-padding: 50px;
-display: flex;
-`
+  padding: 50px;
+  display: flex;
+`;
 const ImgContainer = styled.div`
-flex: 1;
-`
+  flex: 1;
+`;
 const Image = styled.img`
-width: 100%;
-height: 90vh;
-object-fit: cover;
-`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
 const InfoContainer = styled.div`
-flex: 1;
-padding: 0px 50px;
-`
+  flex: 1;
+  padding: 0px 50px;
+`;
 const Title = styled.h1`
-font-weight: 200;
-
-`
+  font-weight: 200;
+`;
 const Desc = styled.p`
-margin: 20px 0px;
-
-`
+  margin: 20px 0px;
+`;
 const Price = styled.span`
-font-weight: 100;
-font-size: 40px;
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+width: 50%;
+margin: 30px 0px;
+display: flex;
+justify-content: space-between;
 `
 
+const Filter = styled.div`
+display: flex;
+align-items: center;
 
+`
 
+const FilterTitle = styled.span`
+font-size: 20px;
+font-weight: 200;
+`
+
+const FilterColor = styled.div`
+width: 20px;
+height: 20px;
+border-radius: 50%;
+background-color: ${props => props.color};
+margin: 0px 5px;
+cursor: pointer;
+`
+
+const FilterSize = styled.select`
+margin-left: 10px;
+`
+
+const FilterSizeOption = styled.option``
 
 const Product = () => {
   return (
-   <Container>
-    <Navbar/>
-    <Annoucement/>
-    <Wrapper>
+    <Container>
+      <Navbar />
+      <Annoucement />
+      <Wrapper>
         <ImgContainer>
-        <Image src='https://i.ibb.co/S6qMxwr/jean.jpg'/>
+          <Image src='https://i.ibb.co/S6qMxwr/jean.jpg' />
         </ImgContainer>
         <InfoContainer>
-            <Title> Denim Jumpsuit</Title>
-            <Desc> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Totam numquam deserunt pariatur porro fugit libero mollitia sed temporibus
-                 fuga saepe aperiam sit commodi tenetur at, necessitatibus nesciunt eaque! 
-                 Dolorum, architecto!</Desc>
-            <Price>$ 20</Price>
-        </InfoContainer>
-    </Wrapper>
-    <Newsletter/>
-    <Footer/>
-   </Container>
-  )
-}
+          <Title> Denim Jumpsuit</Title>
+          <Desc>
+            {' '}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            numquam deserunt pariatur porro fugit libero mollitia sed temporibus
+            fuga saepe aperiam sit commodi tenetur at, necessitatibus nesciunt
+            eaque! Dolorum, architecto!
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+                <FilterTitle>Color</FilterTitle>
+                <FilterColor color='black'/>
+                <FilterColor color='darkblue'/>
+                <FilterColor color='gray'/>
+            </Filter>
+            <Filter>
+                <FilterTitle>Size</FilterTitle>
+                <FilterSize>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                    <FilterSizeOption>S</FilterSizeOption>
+                    <FilterSizeOption>M</FilterSizeOption>
+                    <FilterSizeOption>L</FilterSizeOption>
+                    <FilterSizeOption>XL</FilterSizeOption>
+                </FilterSize>
+            </Filter>
 
-export default Product
+          </FilterContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
