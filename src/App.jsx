26fa8f768a642e34c7c3fc244ef6139ce8a85c95
Rafael Martins <rmartins858@gmail.com>
2022-07-27 18:47:57 -0300

    Register Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 // import Home from "./page/Home";
 // import ProductList from "./page/ProductList";
-
-import Product from "./page/Product";
+//import Product from "./page/Product";
+import Login from "./page/Login";
+import Register from "./page/Register";
 
 const App = () => {
-  return <Product/>
+  return <Login/>
 };
 
 export default App;
