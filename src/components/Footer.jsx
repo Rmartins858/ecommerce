3c05ce185600ac4b897ea2f0dd347cb5e2783809
Rafael Martins <rmartins858@gmail.com>
2022-07-27 15:57:13 -0300

    implementação no Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,137 +1,132 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import MapIcon from '@mui/icons-material/Map';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 
-
 const Container = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 const Left = styled.div`
-flex:1;
-display: flex;
-flex-direction: column;
-padding: 20px;
-
-`
-const Logo = styled.h1``
-
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
 
 const Desc = styled.p`
-margin: 20px 0px;
-`
-
+  margin: 20px 0px;
+`;
 
 const SocialContainer = styled.div`
-display: flex;
-`
-
+  display: flex;
+`;
 
 const SocialIcon = styled.div`
-width: 40px;
-height: 40px;
-border-radius: 50%;
-color: white;
-background-color: #${props=>props.color};
-display: flex;
-align-items: center;
-justify-content: center;
-margin: 20px;
-`
-
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+`;
 
 const Center = styled.div`
-flex: 1;
-padding: 20px;
-`
+  flex: 1;
+  padding: 20px;
+`;
 const Right = styled.div`
-flex: 1;
-padding: 20px;
-`
+  flex: 1;
+  padding: 20px;
+`;
 
-const Title =  styled.h3`
-    margin-bottom: 30px;
-`
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
 
 const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const ListItem = styled.li`
-    width: 50%;
-    margin-bottom: 10px;
-`
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`;
 
-`
-
-
-const Payment = styled.div``
- 
+const Payment = styled.img`
+width: 50%;
+`;
 
 const Footer = () => {
   return (
-   <Container>
-
-    <Left>
+    <Container>
+      <Left>
         <Logo>RAFA.</Logo>
-        <Desc>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Quam quis nisi hic tenetur inventore iste cupiditate quia quidem a similique!
-              Quia consequuntur dolorem consequatur nostrum natus magni commodi, voluptate officia.</Desc>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis
+          nisi hic tenetur inventore iste cupiditate quia quidem a similique!
+          Quia consequuntur dolorem consequatur nostrum natus magni commodi,
+          voluptate officia.
+        </Desc>
         <SocialContainer>
-            <SocialIcon color="3B5999">
-                <FacebookIcon/>
-            </SocialIcon>
-            <SocialIcon color="E4405f">
-                <InstagramIcon/>
-            </SocialIcon>
+          <SocialIcon color='3B5999'>
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color='E4405f'>
+            <InstagramIcon />
+          </SocialIcon>
         </SocialContainer>
-    </Left>
-    <Center>
-
+      </Left>
+      <Center>
         <Title>Useful Links</Title>
 
         <List>
-            <ListItem>Home</ListItem>
-            <ListItem>Cart</ListItem>
-            <ListItem>Man Fashion</ListItem>
-            <ListItem>Woman Fashion</ListItem>
-            <ListItem>Accessories</ListItem>
-            <ListItem>My Account</ListItem>
-            <ListItem>Order Tracking</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Terms</ListItem>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
         </List>
-    </Center>
-    <Right>
+      </Center>
+      <Right>
         <Title>Contact</Title>
         <ContactItem>
-            <MapIcon/>
-        Lorem ipsum dolor sit amet consectetur
+          <MapIcon style={{marginRight:'10px'}}/>
+          Lorem ipsum dolor sit amet consectetur
         </ContactItem>
 
         <ContactItem>
-            <PhoneIcon/>
-        21 91234-5678
+          <PhoneIcon  style={{marginRight:'10px'}} />
+          21 91234-5678
         </ContactItem>
 
         <ContactItem>
-            <EmailIcon/>
-        contact@rafa.dev
+          <EmailIcon  style={{marginRight:'10px'}} />
+          contact@rafa.dev
         </ContactItem>
-    </Right>
+      <Payment src='https://i.ibb.co/Qfvn4z6/pay,ent.png' />
+      </Right>
+    </Container>
+  );
+};
 
-    <Payment src='https://i.ibb.co/Qfvn4z6/pay,ent.png'/>
-     
-   </Container>
-  )
-}
-
-export default Footer
+export default Footer;
