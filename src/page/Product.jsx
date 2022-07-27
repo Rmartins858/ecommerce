2b974af2172fdc93e982ea1cfb905253b4ef0b36
Rafael Martins <rmartins858@gmail.com>
2022-07-27 17:41:52 -0300

    button styling

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
+import { Add, Remove } from '@material-ui/icons';
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -33,37 +34,73 @@ const Price = styled.span`
 `;
 
 const FilterContainer = styled.div`
-width: 50%;
-margin: 30px 0px;
-display: flex;
-justify-content: space-between;
-`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Filter = styled.div`
-display: flex;
-align-items: center;
-
-`
+  display: flex;
+  align-items: center;
+`;
 
 const FilterTitle = styled.span`
-font-size: 20px;
-font-weight: 200;
-`
+  font-size: 20px;
+  font-weight: 200;
+`;
 
 const FilterColor = styled.div`
-width: 20px;
-height: 20px;
-border-radius: 50%;
-background-color: ${props => props.color};
-margin: 0px 5px;
-cursor: pointer;
-`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
 
 const FilterSize = styled.select`
-margin-left: 10px;
-`
+  margin-left: 10px;
+  padding: 5px;
+`;
 
-const FilterSizeOption = styled.option``
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
 
 const Product = () => {
   return (
@@ -86,23 +123,30 @@ const Product = () => {
           <Price>$ 20</Price>
           <FilterContainer>
             <Filter>
-                <FilterTitle>Color</FilterTitle>
-                <FilterColor color='black'/>
-                <FilterColor color='darkblue'/>
-                <FilterColor color='gray'/>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color='black' />
+              <FilterColor color='darkblue' />
+              <FilterColor color='gray' />
             </Filter>
             <Filter>
-                <FilterTitle>Size</FilterTitle>
-                <FilterSize>
-                    <FilterSizeOption>XS</FilterSizeOption>
-                    <FilterSizeOption>S</FilterSizeOption>
-                    <FilterSizeOption>M</FilterSizeOption>
-                    <FilterSizeOption>L</FilterSizeOption>
-                    <FilterSizeOption>XL</FilterSizeOption>
-                </FilterSize>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
             </Filter>
-
           </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
